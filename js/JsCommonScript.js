372025e33dccfb0d4913403ad3449f028a4bd6a3
Ubaid <khanxx777@gmail.com>
2022-10-06 17:04:14 +0500

    ubaid

---
+++ js/JsCommonScript.js
@@ -0,0 +1,10 @@
+// const ElIdNetworkBenchmarkTesting = document.getElementById(
+//   "IdNetworkBenchmarkTesting"
+// );
+// const ElIdFieldPerformanceTesting = document.getElementById(
+//   "IdFieldPerformanceTesting"
+// );
+
+const FnSetServiceId = (serciceId) => {
+  sessionStorage.setItem("service", serciceId);
+};
